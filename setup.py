d89d0d20c7f8b236eed5ef55c01b961a0fdf014e
Jonas Baumann <j.baumann@4teamwork.ch>
2010-05-07 11:47:59 +0000

    Added z3c.autoinclude for zcml-dependencies

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open('ftw/publisher/receiver/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
 
 setup(name='ftw.publisher.receiver',
       version=version,
-      description="Receiver package for publisher product",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="Receiver package for publisher product" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='ftw publisher receiver',
-      author='Jonas Baumann',
-      author_email='j.baumann@4teamwork.ch',
-      url='https://svn.4teamwork.ch/repos/ftw/ftw.publisher.receiver/',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.publisher.receiver/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'z3c.autoinclude',
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
