95fc170c5810691e9aea5578e4eae20522b7e554
Julian Infanger <j.infanger@4teamwork.ch>
2012-11-26 12:20:46 +0100

    Removed archetypes.schemaextender, since we can use obj.Schema() for getting.

--- ftw/publisher/receiver/decoder.py
+++ ftw/publisher/receiver/decoder.py
@@ -10,15 +10,6 @@ import base64
 import simplejson
 
 
-#make archetype.schemaextender aware
-HAS_AT_SCHEMAEXTENDER = False
-try:
-    from archetypes.schemaextender.interfaces import ISchemaExtender
-    HAS_AT_SCHEMAEXTENDER = True
-except ImportError:
-    pass
-
-
 class Decoder(object):
     """
     Decodes json data to dictionary and validates it.
@@ -119,13 +110,7 @@ class Decoder(object):
         if not isinstance(self.data[jsonkey],dict):
             return self.data
 
-        schema = self.getSchema(object)
-        fields = []
-
-        if schema is not None:
-            fields = schema.fields()
-        if HAS_AT_SCHEMAEXTENDER and queryAdapter(object, ISchemaExtender):
-            fields += ISchemaExtender(object).getFields()
+        fields = self.object.Schema().fields()
 
         for field in fields:
             name = field.getName()
