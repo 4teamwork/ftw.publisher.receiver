c4d2d94247f23a1f407a8421f9753fa7471d872c
Jonas Baumann <jone@jone.ch>
2015-09-15 12:16:13 +0200

    add plone.app.testing layer with ftw.builder.

--- ftw/publisher/receiver/testing.py
+++ ftw/publisher/receiver/testing.py
@@ -1,4 +1,28 @@
+from zope.configuration import xmlconfig
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from ftw.builder.testing import BUILDER_LAYER
 from collective.testcaselayer.ptc import BasePTCLayer, ptc_layer
+
+
+class ReceiverLayer(PloneSandboxLayer):
+    defaultBases = (PLONE_FIXTURE, BUILDER_LAYER)
+
+    def setUpZope(self, app, configurationContext):
+        xmlconfig.string(
+            '<configure xmlns="http://namespaces.zope.org/zope">'
+            '  <include package="z3c.autoinclude" file="meta.zcml" />'
+            '  <includePlugins package="plone" />'
+            '  <includePluginsOverrides package="plone" />'
+            '</configure>',
+            context=configurationContext)
+
+
+RECEIVER_FIXTURE = ReceiverLayer()
+RECEIVER_INTEGRATION = IntegrationTesting(
+    bases=(RECEIVER_FIXTURE, ),
+    name="ftw.publisher.receiver:integration")
 
 
 class ReceiverIntegrationLayer(BasePTCLayer):
