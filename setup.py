28ef064385938d06f75ba4f13332c4a7e2d8f403
Martin Bächtold <martin@baechtold.me>
2018-02-14 15:32:16 +0100

    Test against Plone 5.1.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(name='ftw.publisher.receiver',
       classifiers=[
           'Framework :: Plone',
           'Framework :: Plone :: 4.3',
+          'Framework :: Plone :: 5.1',
           'Programming Language :: Python',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],
